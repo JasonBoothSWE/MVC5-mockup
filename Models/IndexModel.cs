615e412fb10d545a85f99bf8e1bdef57736ffce9
JasonBoothSWE <73965452+JasonBoothSWE@users.noreply.github.com>
2023-01-10 15:02:35 -0600

    continued work with membership

---
+++ Models/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC5_mockup.Models
+{
+    public class IndexModel
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MVC5_mockup.Models
-{
-    public class LoginViewModel
-    {
-
-    }
-}
