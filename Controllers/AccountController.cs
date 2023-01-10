bb52823e8c0edfef0823fe76cbeab02726cae372
JasonBoothSWE <121974603+JasonBoothLTG@users.noreply.github.com>
2023-01-10 12:19:50 -0600

    created login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC5_mockup.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
